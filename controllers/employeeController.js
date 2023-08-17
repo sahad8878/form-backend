096a19a75549a50037890ebdf86237847b4ac378
sahad <msahadpp@gmail.com>
2023-08-17 18:45:32 +0530

    first commit

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -1,8 +1,5 @@
-
-import Employee from '../models/employeeModel.js'
+import Employee from "../models/employeeModel.js";
 export const postEmployeeDetails = async (req, res) => {
-  console.log(req.body, "data");
-
   const {
     name,
     mobile,
@@ -26,7 +23,18 @@ export const postEmployeeDetails = async (req, res) => {
     gender &&
     experiences
   ) {
-    const newExperiences = experiences.map(exp => {
+    const existingEmployee = await Employee.findOne({ email: email });
+    if (existingEmployee) {
+      res.json({
+        success: false,
+        message: `${email} employee is already exists`,
+      });
+      return;
+    }else{
+
+
+
+      const newExperiences = experiences.map((exp) => {
         const timePeriod = `${exp.startYear} - ${exp.endYear}`;
         return {
           company: exp.company,
@@ -34,27 +42,27 @@ export const postEmployeeDetails = async (req, res) => {
           timePeriod: timePeriod,
         };
       });
-    const employee = await Employee.create({
+      const employee = await Employee.create({
         name,
-    mobile,
-    email,
-    employeeId,
-    address,
-    designation,
-    joiningDate,
-    gender,
-    experiences:newExperiences,
+        mobile,
+        email,
+        employeeId,
+        address,
+        designation,
+        joiningDate,
+        gender,
+        experiences: newExperiences,
       });
       if (employee) {
         res.status(201).json({
-     success:true,message:"successfully created employee details"
+          success: true,
+          message: "successfully created employee details",
         });
       } else {
-        res.json({success:false,message:"employee not found"})
+        res.json({ success: false, message: "employee not found" });
       }
-
-  }else {
- res.json({success:false,message:"Please enter all the fields"})
-
+    }
+  } else {
+    res.json({ success: false, message: "Please enter all the fields" });
   }
 };
